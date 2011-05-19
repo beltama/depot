250cb12c8346d5e86280cf4bc037bdb783d22cfb
Angel Villaverde <beltama2@hotmail.com>
2011-05-19 16:17:28 +0100

    email failing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,15 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain => "gmail.com",
+	:authentication => "plain",
+	:user_name => "chonapochona",
+	:password => "53278805h",
+	:enable_starttls_auto => true
+	}
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain => "gmail.com",
+	:authentication => "plain",
+	:user_name => "chonapochona",
+	:password => "53278805h",
+	:enable_starttls_auto => true
+	}
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true

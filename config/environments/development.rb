f4c0431798dbd29ce707ae737cb0168710101b4a
Angel Villaverde <beltama2@hotmail.com>
2011-05-24 16:12:39 +0100

    users perfect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Depot::Application.configure do
   	:domain => "gmail.com",
   	:authentication => "plain",
   	:user_name => "chonapochona",
-  	:password => "********",
+  	:password => "******",
   	:enable_starttls_auto => true
   	}
 
